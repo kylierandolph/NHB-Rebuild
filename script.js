562444377d492c053d63ee4c0bc29d215cfbfc12
Kylie Randolph <kylie.r.randolph@gmail.com>
2017-07-18 19:39:49 -0400

    Added modal comments

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
 var name = alert("Ready to try something new?")
-
-// form method="post" id="button" action="index.html" type="submit" onSubmit="alert('You're one step closer to trying something new!');"
