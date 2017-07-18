5f23edd3f39cebad6909fe4ad92ca8c5b4f1fe95
Kylie Randolph <kylie.r.randolph@gmail.com>
2017-07-18 18:21:28 -0400

    Fixing the movie issue

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
+var name = alert("Ready to try something new?")
 
+// form method="post" id="button" action="index.html" type="submit" onSubmit="alert('You're one step closer to trying something new!');"
