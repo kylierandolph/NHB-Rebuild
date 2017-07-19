089e8223a8e5df5788a64f6d6ade9bb97ec022b2
Kylie Randolph <kylie.r.randolph@gmail.com>
2017-07-18 22:47:12 -0400

    Added scroll to top button

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-var name = alert("Ready to try something new?")
+
+// Message when Sign up button is submitted-->
+var name = alert("You're one step closer to learning something new!")
+
+// Back to top button -->
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// Scrolls to top when selected -->
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
